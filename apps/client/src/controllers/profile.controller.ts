dd7be7f770e2890192299747c879f4504622d138
hadyRashwan <h2rashwan@gmail.com>
2019-12-12 15:49:02 +0200

    adding name in the get profile request

--- apps/client/src/controllers/profile.controller.ts
+++ apps/client/src/controllers/profile.controller.ts
@@ -52,6 +52,7 @@ const getClient = async (wellKnownMap, i, uid, host) => {
   await saveDB({ code_verifier, code_challenge, uid, state }, 'state');
 
   i.authUrl = authUrl;
+  i.name = wellKnownMap[i._id].badgeConnectAPI[0].name
   return i ;
 };
 export const generate = async (req, res) => {
