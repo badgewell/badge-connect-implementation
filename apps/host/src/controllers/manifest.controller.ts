d7ff39197bf1189c3acfa041231d8d4db0e48093
hadyRashwan <h2rashwan@gmail.com>
2020-01-30 16:53:47 +0200

    removing not used wellKnow scopes

--- apps/host/src/controllers/manifest.controller.ts
+++ apps/host/src/controllers/manifest.controller.ts
@@ -23,11 +23,11 @@ export const wellKnown = (req: Request, res: Response) => {
   const scopes: scope[] = [
     'openid',
     'profile',
-    'https://purl.imsglobal.org/spec/ob/v2p1/scope/assertion.readonly',
-    'https://purl.imsglobal.org/spec/ob/v2p1/scope/assertion.create',
-    'https://purl.imsglobal.org/spec/ob/v2p1/scope/profile.readonly',
-    'https://purl.imsglobal.org/spec/ob/v2p1/scope/profile.update',
-    'offline_access'
+    'https://purl.imsglobal.org/spec/ob/v2p1/scope/assertion.readonly'
+    // 'https://purl.imsglobal.org/spec/ob/v2p1/scope/assertion.create',
+    // 'https://purl.imsglobal.org/spec/ob/v2p1/scope/profile.readonly',
+    // 'https://purl.imsglobal.org/spec/ob/v2p1/scope/profile.update',
+    // 'offline_access'
   ];
   const { host } = req.headers;
   const { secure } = req;
